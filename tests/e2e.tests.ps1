7b310dde307aea8dd76972fe1f9633ac7a6c0362
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-03-15 21:45:08 -0700

    minor updates

--- tests/e2e.tests.ps1
+++ tests/e2e.tests.ps1
@@ -27,7 +27,7 @@ else{
 Describe 'get-nuget tests' {   
     It 'Returns path' {
         $nugetPath = (Get-Nuget)
-        $nugetPath | Should exist        
+        $nugetPath | Should exist
     }
      
     It 'Can use specified url' {
@@ -49,6 +49,79 @@ Describe 'update-nuget tests'{
         $nugetPath = (Update-NuGet)
         $nugetPath | Should Exist
     }
+}
+
+Describe 'get-nugetpackage tests' {
+    $oldToolsDir = $Global:NuGetPowerShellSettings.toolsDir
+    $newToolsDir = (Join-Path $TestDrive 'get-nupkg\newtools\')
+    if(!(Test-Path $newToolsDir)){
+        New-Item $newToolsDir -ItemType Directory | out-null
+    }
+    $newToolsDir = ((Get-Item $newToolsDir).FullName)
+    $Global:NuGetPowerShellSettings.toolsDir = $newToolsDir
+
+    BeforeEach {
+        if(Test-Path $newToolsDir){
+            Remove-Item $newToolsDir -Recurse -Force -ErrorAction SilentlyContinue | out-null
+        }
+    }
+
+    It 'Returns path' {
+        $pkgPath = (Get-NuGetPackage -name psbuild)
+        $pkgPath | Should Exist
+    }
+
+    It 'Folder contains .nupkg file'{
+        $pkgPath = (Get-NuGetPackage -name psbuild -prerelease)
+        [System.IO.FileInfo]$result=(Get-ChildItem $pkgPath psbuild*.nupkg)
+        $result | Should Not Be $null
+        $result.Exists | Should Be $true
+    }
+
+    It 'Can use specified url' {
+        $tempDir = (join-path $TestDrive 'nugetrepo01\')
+        mkdir $tempDir
+        $tempDir = ((Get-Item $tempDir).FullName)
+
+        # get a nuget pkg
+        $pkgPath = (Get-NuGetPackage -name psbuild)
+        Get-ChildItem $pkgPath *.nupkg | % {Copy-Item $_.FullName -Destination $tempDir}
+
+       'files:'|Write-host
+        get-childitem $tempDir * -Recurse | Write-Host
+
+        $newPkgPath = (Get-NuGetPackage -name psbuild -nugetUrl "$tempDir")
+        $newPkgPath | Should Exist
+    }
+
+    It 'Returns path when already downloaded' {
+        $pkgPath = (Get-NuGetPackage -name psbuild)
+        $pkgPath | Should Exist
+        $pkgPath = (Get-NuGetPackage -name psbuild)
+        $pkgPath | Should Exist
+    }
+
+    It 'Can pass in a specific version' {
+        $pkgPath = (Get-NuGetPackage -name psbuild -version 0.0.1)
+        $pkgPath | Should Exist
+    }
+
+    It 'Can pass in prerelase' {
+        $pkgPath = (Get-NuGetPackage -name psbuild -prerelease)
+        $pkgPath | Should Exist
+    }
+
+    It 'Can pass in force without passing version' {
+        $pkgPath = (Get-NuGetPackage -name psbuild -force)
+        $pkgPath | Should Exist
+    }
+
+    It 'Can pass in force with passing version' {
+        $pkgPath = (Get-NuGetPackage -name psbuild -version 0.0.1 -force)
+        $pkgPath | Should Exist
+    }
+
+    $Global:NuGetPowerShellSettings.toolsDir = $oldToolsDir
 }
 
 Describe 'Load-ModuleFromNuGetPackage tests'{
