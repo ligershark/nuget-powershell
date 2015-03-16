956150520b2de8cef162b1ac8b5e60f3450bd183
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-03-15 17:42:49 -0700

    mostly adding help

--- nuget-powershell.psm1
+++ nuget-powershell.psm1
@@ -1,6 +1,9 @@
 <#
 .SYNOPSIS
-    This module aimes to help similify consuming NuGet packages from PowerShell.
+    This module aimes to help similify consuming NuGet packages from PowerShell. To find the commands
+    made available you can use.
+
+    Get-Command -module nuget-powershell
 #>
 
 function Get-ScriptDirectory
@@ -55,7 +58,7 @@ function Get-Nuget{
 
 <#
 .SYNOPSIS
-Updates nuget.exe to the latest and then returns the path to nuget.exe.
+    Updates nuget.exe to the latest and then returns the path to nuget.exe.
 #>
 function Update-NuGet{
     [cmdletbinding()]
@@ -71,6 +74,25 @@ function Update-NuGet{
     }
 }
 
+<#
+.SYNOPSIS
+    Used to execute a command line tool (i.e. nuget.exe) using cmd.exe. This is needed in
+    some cases due to hanlding of special characters.
+
+.EXAMPLE
+    Execute-CommandString -command ('"{0}" {1}' -f (Get-NuGet),(@('install','psbuild') -join ' '))
+    Calls nuget.exe install psbuild using cmd.exe
+
+.EXAMPLE
+    '"{0}" {1}' -f (Get-NuGet),(@('install','psbuild') -join ' ') | Execute-CommandString
+    Calls nuget.exe install psbuild using cmd.exe
+
+.EXAMPLE
+    @('psbuild','packageweb') | % { """$(Get-NuGet)"" install $_ -prerelease"|Execute-CommandString}
+    Calls 
+        nuget.exe install psbuild -prerelease
+        nuget.exe install packageweb -prerelease
+#>
 function Execute-CommandString{
     [cmdletbinding()]
     param(
@@ -95,7 +117,51 @@ function Execute-CommandString{
 
 <#
 .SYNOPSIS
-    This will return the path to where the given NuGet package is installed.
+    This will return the path to where the given NuGet package is installed. If the package
+    is not in the local cache then it will automatically be downloaded. All interaction with
+    nuget servers go through nuget.exe.
+
+.PARAMETER name
+    Name of the NuGet package to be installed. This is a mandatory argument.
+
+.PARAMETER version
+    Version of NuGet package to get. If this is not passed the latst version will be
+    returned. That will result in a call to nuget.org (or other nugetUrl as specified).
+
+.PARAMETER prerelease
+    Pass this to get the prerelease version of the NuGet package.
+
+.PARAMETER toolsDir
+    The directory where the package will be downloaded to. This is mostly an internal
+    parameter but it can be used to redirect the location of the tools directory. 
+    To override this globally you can use $global:NuGetPowerShellSettings.toolsDir.
+
+.PARAMETER nugetUrl
+    You can use this to download the package from a different nuget feed.
+
+.PARAMETER force
+    Used to re-download the package from the remote nuget feed.
+
+.EXAMPLE
+    Get-NuGetPackage -name psbuild
+    Gets the latest version of the psbuild nuget package wich is not a prerelease package
+
+.EXAMPLE
+    Get-NuGetPackage -name psbuild -prerelease
+    Gets the latest version (including prerelase) of the psbuild nuget package.
+
+.EXAMPLE
+    Get-NuGetPackage psbuild -version 0.0.5
+    Gets psbuild version 0.0.5
+
+.EXAMPLE
+    Get-NuGetPackage psbuild -version 0.0.6-beta5
+    Gets psbuild version 0.0.6-beta5. When passing a value for version you don't need
+    to pass -prerelease, it will be used by default on all calls when version is present.
+
+.EXAMPLE
+    Get-NuGetPackage psbuild -version 0.0.6-beta5 -nugetUrl https://staging.nuget.org
+    Downloads psbuild version 0.0.6-beta5 fro staging.nuget.org
 #>
 function Get-NuGetPackage{
     [cmdletbinding()]
@@ -110,7 +176,10 @@ function Get-NuGetPackage{
         $toolsDir = $global:NuGetPowerShellSettings.toolsDir,
 
         [Parameter(Position=4)]
-        [string]$nugetUrl = $null
+        [string]$nugetUrl = $null,
+
+        [Parameter(Position=5)]
+        [switch]$force
     )
     process{
         if(!(Test-Path $toolsDir)){
@@ -120,9 +189,13 @@ function Get-NuGetPackage{
         # if it's already installed just return the path
         [string]$installPath = $null
 
-
         if($version){
             $installPath = (InternalGet-NuGetPackageExpectedPath -name $name -version $version -toolsDir $toolsDir)
+        }
+
+        if($force -and $installPath -and (Test-Path $installPath)){
+            'Deleting package directory at [{0}] because of -force' -f $installPath | Write-Verbose
+            Remove-Item $installPath -Recurse -Force | Write-Verbose
         }
 
         if(!$installPath -or !(test-path $installPath)){
@@ -160,6 +233,15 @@ function Get-NuGetPackage{
                     $pkgDirName = InternalGet-PackagePathFromNuGetOutput -nugetOutput ($nugetResult[0])
                     $pkgpath = (Join-Path $toolsDir $pkgDirName)
                     $installPath = ((Get-Item $pkgpath).FullName)
+
+                    # if the version is not passed with -force then the item may be downloaded twice
+                    if($force -and !($version)){
+                        'Deleting package directory at [{0}] because of -force' -f $installPath | Write-Verbose
+                        Remove-Item $installPath -Recurse -Force | Write-Verbose
+                    }
+
+                    [string[]]$nugetResult = (Execute-CommandString -command $nugetCommand)
+                    $nugetResult | Write-Verbose
                 }
             }
             finally{
@@ -197,22 +279,47 @@ function InternalGet-PackagePathFromNuGetOutput{
     }
 }
 
+<#
+.SYNOPSIS
+    When this method is called all files in the given nuget package maching *.psm1 in the tools
+    folder will automatically be imported using Import-Module.
+
+.EXAMPLE
+    Load-ModuleFromNuGetPackage -name psbuild
+    Loads the psbuild module from the latest psbuild nuget package (non-prerelease).
+
+.EXAMPLE
+    Load-ModuleFromNuGetPackage -name psbuild -prerelease
+    Loads the psbuild module from the latest psbuild nuget package (including prerelease).
+
+.EXAMPLE
+    Load-ModuleFromNuGetPackage -name psbuild -prerelease -force
+    Loads the psbuild module from the latest psbuild nuget package (including prerelease), and the package
+    will be re-dowloaded instead of the cached version.
+#>
 function Load-ModuleFromNuGetPackage{
     [cmdletbinding()]
     param(
-        [Parameter(Mandatory=$true,Position=0)]
+        [Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)]
         $name,
 
-        [Parameter(Mandatory=$true,Position=1)] # later we can make this optional
+        [Parameter(Position=1)]
         $version,
+
         [Parameter(Position=2)]
-        $toolsDir = $global:NuGetPowerShellSettings.toolsDir,
+        [switch]$prerelease,
 
         [Parameter(Position=3)]
-        $nugetUrl = $null
+        $toolsDir = $global:NuGetPowerShellSettings.toolsDir,
+
+        [Parameter(Position=4)]
+        $nugetUrl = $null,
+
+        [Parameter(Position=5)]
+        [switch]$force
     )
     process{
-        $installDir = Get-NuGetPackage -name $name -version $version -nugetUrl $nugetUrl
+        $installDir = Get-NuGetPackage -name $name -version $version -prerelease:$prerelease -nugetUrl $nugetUrl -force:$force
 
         $modules = (Get-ChildItem ("$installDir\tools") '*.psm1')
         foreach($module in $modules){
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 This project aims to simplify consuming NuGet packages in PowerShell. More info coming soon...
 
+<code style="background-color:grey">(new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ligershark/nuget-powershell/master/get-nugetps.ps1") | iex</code>
+
 [![Build status](https://ci.appveyor.com/api/projects/status/yv76i6yybh5pv3kd?svg=true)](https://ci.appveyor.com/project/sayedihashimi/nuget-powershell)
